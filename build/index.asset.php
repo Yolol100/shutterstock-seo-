d119b9f627571de942feb0ce3ea989e4aa086db3
Yolol100 <Andrew_Baeten@hotmail.com>
2026-05-24 05:43:17 +0700

    test

--- build/index.asset.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-return array(
-	'dependencies' => array( 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n' ),
-	'version'      => '1.3.7',
-);
